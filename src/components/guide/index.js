d03c0776fa7e84767f79594e526a67ba04c0b933
Steven Shum <shenweiyan@aliyun.com>
2025-09-09 14:07:27 +0800

    Update index.js

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -47,18 +47,38 @@ export default class Guide extends React.Component {
             <em style={{ [transform]: 'translate(7px,3px)rotate(90deg) scale(1,2)' }} />
           </div>
         </div>
-        <div className={`${style.guide} ${style.left}`}>    
+        <div className={`${style.guide} ${style.left}`}>
+          <p>
+            <a href="https://github.com/chvin/react-tetris" rel="noopener noreferrer" target="_blank" title={i18n.linkTitle[lan]}>{`${i18n.github[lan]}:`}</a><br />
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=chvin&repo=react-tetris&type=star&count=true"
+              frameBorder="0"
+              scrolling="0"
+              width="170px"
+              height="20px"
+              style={{ [transform]: 'scale(1.68)', [`${transform}Origin`]: 'center left' }}
+            />
+            <br />
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=chvin&repo=react-tetris&type=fork&count=true"
+              frameBorder="0"
+              scrolling="0"
+              width="170px"
+              height="20px"
+              style={{ [transform]: 'scale(1.68)', [`${transform}Origin`]: 'center left' }}
+            />
+          </p>
           <div className={style.space}>SPACE</div>
         </div>
         { this.state.QRCode !== '' ? (
-          <div style="display:none" className={`${style.guide} ${style.qr}`}>
+          <!--div className={`${style.guide} ${style.qr}`}>
             <img
               src={this.state.QRCode}
               alt={i18n.QRCode[lan]}
             />
           </div>
         ) : null }
-      </div>
+      </div-->
     );
   }
 }
